676fa86ee8e6bed7fb2c119e78818321caa5a78b
Pablo <larrazjp@gmail.com>
2024-11-18 12:08:07 +0100

    new abgabe

--- public/javascripts/chess.js
+++ public/javascripts/chess.js
@@ -21,11 +21,34 @@ $(document).ready(function() {
             });
         }
     });
-
+    connectWebSocket()
     // Initial board load
     loadBoard();
 });
+function connectWebSocket() {
+    var websocket = new WebSocket("ws://localhost:9000/websocket");
+    websocket.setTimeout
 
+    websocket.onopen = function(event) {
+        console.log("Connected to Websocket");
+    }
+
+    websocket.onclose = function () {
+        console.log('Connection with Websocket Closed!');
+    };
+
+    websocket.onerror = function (error) {
+        console.log('Error in Websocket Occured: ' + error);
+    };
+
+    websocket.onmessage = function (e) {
+        if (typeof e.data === "string") {
+            let json = JSON.parse(e.data);
+            //do something
+        }
+
+    };
+}
 function loadBoard() {
     $.ajax({
         url: "/jsonGame", // Assuming this endpoint returns the current board state
